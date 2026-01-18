90cdd2a3b03cbe749860d13f3ff8e02164d2bf98
dbshadow <dbshadow@gmail.com>
2026-01-18 21:15:03 +0800

    fix for wrong color of search block.
     Changes to be committed:
    	modified:   web/src/views/HomeView.vue

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -143,7 +143,7 @@ onMounted(() => {
           <input 
             v-model="searchQuery"
             type="text" 
-            class="block w-full rounded-2xl border-0 py-3.5 pl-11 pr-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 transition-all" 
+            class="block w-full rounded-2xl border-0 bg-white py-3.5 pl-11 pr-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 transition-all" 
             :placeholder="t('home.searchPlaceholder')" 
           />
         </div>
